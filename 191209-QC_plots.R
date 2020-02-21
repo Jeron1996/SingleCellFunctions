9e9c0455aa155074640abb639f832660369f508f
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-02-21 11:20:26 +1100

    Scripts used in workflow

---
+++ 191209-QC_plots.R
@@ -0,0 +1,116 @@
+###Quality Control Summary
+###Creates multiple plots to show the quality of the emptydrop result
+
+library(ggplot2)
+library(Seurat)
+
+
+###################################################################################################################################
+####################################################QC plots emptydrop workflow####################################################
+###################################################################################################################################
+
+QC_plots_emptydrop <- function(seuratObj.ok, seuratObj.empty, low = low, pro.name, FDR, saveDir){
+  
+  ##Format Seurat Objects & Calculate percent mt
+  seuratObj.ok$orig.ident <- "Ok"
+  seuratObj.empty$orig.ident <- "Empty"
+  
+  seurat.combine <- merge(x = seuratObj.ok, y = seuratObj.empty, add.cell.ids = c("Ok", "Empty"), project = "QC_plots")  
+  
+  ##Create boxplot for nCount_RNA
+  df.count.ok <- data.frame(seuratObj.ok$nCount_RNA, "Ok")
+  df.count.empty <- data.frame(seuratObj.empty$nCount_RNA, "Empty")
+  
+  colnames(df.count.ok) <- c("Counts", "Droplet_state")
+  colnames(df.count.empty) <- c("Counts", "Droplet_state")
+  
+  df.count.comb <- rbind(df.count.ok, df.count.empty)
+  df.count.comb$Droplet_state <- factor(df.count.comb$Droplet_state, levels = c("Empty", "Ok"))
+  xlabel.count <- paste0("Number of ok cells: ", nrow(df.count.comb[df.count.comb$Droplet_state == "Ok", ]), 
+                         "; Number of empty droplets: ", nrow(df.count.comb[df.count.comb$Droplet_state == "Empty",]))
+  box_count <- ggplot(data = df.count.comb, aes(x = Droplet_state, y = Counts)) + geom_boxplot() + facet_zoom(ylim = c(0, 500)) +
+                          theme_bw() + xlab(label = xlabel.count)
+  
+  ##Create boxplot for nFeature_RNA
+  df.feat.ok <- data.frame(seuratObj.ok$nFeature_RNA, "Ok")
+  df.feat.empty <- data.frame(seuratObj.empty$nFeature_RNA, "Empty")
+  
+  colnames(df.feat.ok) <- c("Features", "Droplet_state")
+  colnames(df.feat.empty) <- c("Features", "Droplet_state")
+  
+  df.feat.comb <- rbind(df.feat.ok, df.feat.empty)
+  df.feat.comb$Droplet_state <- factor(df.feat.comb$Droplet_state, levels = c("Empty", "Ok"))
+  
+  xlabel.feat <- paste0("Number of ok cells: ", nrow(df.feat.comb[df.feat.comb$Droplet_state == "Ok", ]),
+                        "; Number of empty droplets: ", nrow(df.feat.comb[df.feat.comb$Droplet_state == "Empty",]))
+  box_feat <- ggplot(data = df.feat.comb, aes(x = Droplet_state, y = Features)) + geom_boxplot() + 
+                        facet_zoom(ylim = c(0, 500)) + theme_bw() +   xlab(label = xlabel.feat)
+  
+  
+  ### Make ViolinPlot comparing mitochondrial gene percentage
+  Vln_mt <- VlnPlot(object = seurat.combine, features = "percent.mt", split.by = "orig.ident", pt.size = 0)
+  
+  
+  ### Save plots in single PDF file
+  pdf.name <- paste0(saveDir, "/", pro.name,"_lower-", low, "_FDR-", FDR, "_emptydrop_QC.pdf")
+  pdf(file = pdf.name)
+  plot(box_feat)
+  plot(box_count)
+  plot(Vln_mt)
+  dev.off()
+  
+  boxplot.list <- list(df.feat.comb, df.feat.comb)
+  names(boxplot.list) <- c("boxplot_data_features", "boxplot_data_counts")
+  list.name <- paste0(saveDir, pro.name,"_lower-", low, "_FDR-", FDR, "_boxplotData.rds")
+  saveRDS(object = boxplot.list, file = list.name)
+  print("Plots saved")
+}
+
+
+###################################################################################################################################
+#####################################################QC plots hashing workflow#####################################################
+###################################################################################################################################
+
+library(Seurat)
+library(ggplot2)
+library(gridExtra)
+library(stringr)
+
+##Make contigency table, ridge plot and tSNE plot to show counts per hash tag
+QC_plots_hashing <- function(hashtag = hashtag, pos.quart = pos.quart, saveDir, name){
+
+  #Idents(hashtag) <- "HTO_maxID"
+  
+  ##make ridge plot for to show expression of each hashtag
+  ridge.plot <- RidgePlot(hashtag, assay = "RNA", features = rownames(hashtag[["RNA"]]), ncol = 2)
+  
+  ##make heatmap of expression values
+  heatmap.hashtag <- HTOHeatmap(hashtag, assay = "HTO", ncells = 5000)
+  
+  ## make tSNE plot for all hashtags except for negative cells
+  hashtag.subset <- subset(hashtag, idents = "Negative", invert = TRUE)
+  #rownames(hashtag.subset) <- substr(x = rownames(hashtag.subset), start = 1, stop = 5)
+  #hto.dist.mtx <- as.matrix(dist(t(GetAssayData(object = hashtag.subset, assay = "HTO"))))
+  #hashtag.subset <- RunTSNE(hashtag.subset, distance.matrix = hto.dist.mtx, perplexity = 100)
+  #dim.plot <- DimPlot(hashtag.subset, label = T, point.size = 1)
+  #dim.plot.global <- DimPlot(hashtag.subset, label = T, group.by = "HTO_classification.global", point.size = 1)
+  
+  ##format contingency table
+  count_table <- table(hashtag$HTO_classification)
+  count_df <- as.data.frame(count_table)
+  count_df <- count_df[base::order(x = count_df[, 2], decreasing = TRUE), ]
+  count_df <- data.frame(count_df[1:(nrow(count_df)/2), ], count_df[((nrow(count_df)/2)+1):nrow(count_df), ])
+  colnames(count_df) <-c("Hashtag" ,"Frequency", "Hashtag" ,"Frequency")
+  
+  ##save as PDF
+  project.name <- hashtag@project.name
+  project.name <- str_replace(project.name, "[[:punct:]]", ",")
+  pos.quart <- str_replace(pos.quart, "[[:punct:]]", ",")
+  
+  ##save as pdf
+  pdf(file = paste0(saveDir, "/", name, "_QuartileCutOff-", pos.quart, "_Plots.pdf"), paper="a4")
+  grid.table(count_df)
+  plot(heatmap.hashtag)
+  plot(ridge.plot)
+  dev.off()
+}
---
+++ 191211-hashing_workflow.R
@@ -0,0 +1,120 @@
+### Simple cell hashing workflow to identify the number of hash-tags and the corresponding number of cells
+
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(Matrix)
+library(stringr)
+
+cell_hashing_workflow <- function(plotting = TRUE, pos.quart = 0.99, seurat.obj.emptydrop, saveDir, umi_dir, hto_dir, pro.name, hash_translation){
+  set.seed(160396)
+  print(umi_dir)
+  print(hto_dir)
+  ##Creates UMI SeuratObject from UMI count matrix and formats it
+  UMI <- Read10X(data.dir = umi_dir, gene.column = 1)
+  rownames(UMI) <- substr(x = rownames(UMI), start = 1, stop = 5)
+  rownaam <- rownames(UMI)
+  rownaam <- rownaam[-length(rownaam)]
+  
+  UMI <- CreateSeuratObject(counts = UMI, project = "Hashing.test", assay = "RNA")
+  UMI <- UMI[rownaam,]
+  VlnPlot(UMI, features = c("nCount_RNA"), pt.size = 0.1)
+  
+  ##Creates HTO SeuratObject from HTO count matrix and formats it
+  HTO <- Read10X(data.dir = hto_dir, gene.column = 1)
+  rownames(HTO) <- substr(x = rownames(HTO), start = 1, stop = 5)
+  df.hto <- as.data.frame(HTO)    ## needed for summary statistic
+  
+  HTO <- CreateSeuratObject(counts = HTO, project = "Hashing.test", assay = "RNA")
+  HTO <- HTO[rownaam, ]
+  VlnPlot(HTO, features = c("nCount_RNA"), pt.size = 0.1)
+  
+  #Create summay for HTO matrix which shows distribution of RNA reads for the different hashtags
+  r <- rownames(df.hto)
+  df <- as.data.frame(matrix(ncol = 0, nrow = ncol(df.hto)))
+  for (i in 1:nrow(df.hto)){
+    df.temp <- as.data.frame(matrix(ncol = 1, nrow = ncol(df.hto)))
+    df.temp[,1] <- as.numeric(df.hto[i, ])
+    colnames(df.temp) <- r[i]
+    df <- cbind(df, df.temp)
+  }
+  print(summary(df))
+  
+  
+  ##filter empty droplets based on e.drops output
+  
+  ok.barcodes <- colnames(seurat.obj.emptydrop)
+  ok.barcodes.clean <- str_remove(string = ok.barcodes, pattern = "-1")
+  UMI <- UMI[, ok.barcodes.clean]
+  HTO <- HTO[, ok.barcodes.clean]
+  n.ok.cells.edrop <- length(ok.barcodes.clean)
+  n.ok.cells.hash <- length(colnames(UMI))
+  print(paste0("Number of ok cells: ", n.ok.cells.edrop, "; Number of cells in UMI: ", n.ok.cells.hash))
+  
+  ##COmbine colnames of UMI and HTO objects to identify cells which pass both filterings
+  joint.colnames <- intersect(colnames(UMI), colnames(HTO))
+  UMI <- UMI[, joint.colnames]
+  #HTO <- HTO[, joint.colnames]
+  HTO <- as.matrix(HTO@assays[["RNA"]]@data[, joint.colnames])
+  
+  #Show which hash.tags are still present. Should be all hash.tags that were present originally
+  print(rownames(UMI))
+
+  
+  ##Seurat hashing workflow
+  hashtag <- UMI
+  
+  ##Normalize using log-normalization
+  hashtag <- NormalizeData(hashtag, verbose = FALSE)
+  hashtag <- FindVariableFeatures(hashtag, selection.method = "mean.var.plot", verbose = FALSE)
+  hashtag <- ScaleData(hashtag, features = VariableFeatures(hashtag), verbose = FALSE)
+
+  #optional using SCTransform normalization instead of log-normalization
+  #hashtag <- SCTransform(hashtag)
+
+  ##Add HTO counts to UMI counts & normalize HTO counts using CLR normalization
+  hashtag[["HTO"]] <- CreateAssayObject(counts = HTO)
+  hashtag <- NormalizeData(object = hashtag, assay = "HTO", normalization.method = "CLR", verbose = FALSE)
+  
+  ##Identify predominat hash.tag for every cell
+  ###play with init, pos.quantile and k.func parameters in HTODemux to tweak selection cutoffs for label calling
+  ###Doesnt matter if we use SCTransform normalization or NormalizeData, did not affect the hash.tag counts in test dataset
+  hashtag <- HTODemux(hashtag, assay = "HTO", positive.quantile = pos.quart)
+  
+  ##Print number of cells annotated as singlet (one hash.tag), doublets (contain multiple hash.tags) or negative (no hash.tag)
+  t <- table(hashtag$HTO_classification.global)
+  print(t)
+  
+  ##create QC plots, if desired
+  
+  if(plotting){
+    saveDir <- saveDir
+    QC_plots_hashing(hashtag = hashtag, pos.quart = pos.quart, saveDir = saveDir, name = pro.name)
+    }
+  ##Add hastag annotation to emptydrop output
+  colna_hash <- paste0(colnames(hashtag), "-1")
+  colna_hash <- sort(colna_hash)
+  colna_empty <- colnames(seurat.obj.emptydrop)
+  colna_empty <- colna_empty[is.element(colna_empty, colna_hash)]
+  colna_empty <- sort(colna_empty)
+  ord.log <<- colna_hash == colna_empty  
+
+  if(sum(ord.log) == length(colna_empty)){
+    seurat.obj.emptydrop <- seurat.obj.emptydrop[, colna_hash]
+    } else{
+      stop("Barcodes not identical or in the same order")
+    }
+  
+  ##Add hashtag data to emptydrop output
+  hashtag <- RenameCells(object = hashtag, new.names = colna_hash)
+  seurat.obj.emptydrop <- AddMetaData(object = seurat.obj.emptydrop, metadata = hashtag@meta.data, col.name = c(colnames(hashtag@meta.data)))
+  
+  ##Return only singlets & translate hashtags
+  seurat.obj.emptydrop <- subset(x = seurat.obj.emptydrop, subset = HTO_classification.global %in% "Singlet")
+  seurat.obj.emptydrop$SampleID <- NA
+  for(i in hash_translation$hash){
+    seurat.obj.emptydrop$SampleID[seurat.obj.emptydrop$HTO_classification %in% i] <- hash_translation[hash_translation$hash %in% i, "hash_trans"]
+  }
+  
+  seurat.obj.emptydrop
+}

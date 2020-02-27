b02a8afd68cb56bd50c1cbf10521d85427b5a589
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-02-28 10:29:17 +1100

    changed directory path

--- 191209-QC_plots.R
+++ 191209-QC_plots.R
@@ -126,19 +126,19 @@ plots_cluster <- function(seurat.object, save.name, dir){
 
   ## Generate and save plots
   Nelbow <- ElbowPlot(object = obj, ndims = 30)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_withSeed_ElbowPlot.pdf"), plot = Nelbow, device = "pdf", width = 10, height = 10)
+  ggsave(filename = paste0(plot_dir, sv.name, "_withSeed_ElbowPlot.pdf"), plot = Nelbow, device = "pdf", width = 10, height = 10)
 
   NsampleID <- DimPlot(object = obj, group.by = "SampleID", label = T)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_SampleID.pdf"), plot = NsampleID, device = "pdf", width = 10, height = 10)
+  ggsave(filename = paste0(plot_dir, sv.name, "_SampleID.pdf"), plot = NsampleID, device = "pdf", width = 10, height = 10)
 
   Norig.ident <- DimPlot(object = obj, group.by = "orig.ident", label = T)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_OrigIdent.pdf"), plot = Norig.ident, device = "pdf", width = 10, height = 10)
+  ggsave(filename = paste0(plot_dir, sv.name, "_OrigIdent.pdf"), plot = Norig.ident, device = "pdf", width = 10, height = 10)
 
   NCellCycle <- DimPlot(object = obj, group.by = "Phase", label = T)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_CellCyclePhase.pdf"), plot = NCellCycle, device = "pdf", width = 10, height = 10)
+  ggsave(filename = paste0(plot_dir, sv.name, "_CellCyclePhase.pdf"), plot = NCellCycle, device = "pdf", width = 10, height = 10)
 
   NpercentMT <- FeaturePlot(object = obj, features = "percent.mt")
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_PercentMT.pdf"), plot = NpercentMT, device = "pdf", width = 10, height = 10)
+  ggsave(filename = paste0(plot_dir, sv.name, "_PercentMT.pdf"), plot = NpercentMT, device = "pdf", width = 10, height = 10)
 
   ## Plot DimPlots for all resolutions
   resolution <- names(obj@meta.data)
@@ -149,20 +149,20 @@ plots_cluster <- function(seurat.object, save.name, dir){
       Idents(obj) <- res
 
       temp_plot <- DimPlot(obj, reduction="umap", label=T)
-      ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, ".pdf"), plot = temp_plot, device = "pdf", width = 10, height = 10)
+      ggsave(filename = paste0(plot_dir, sv.name, "_", res, ".pdf"), plot = temp_plot, device = "pdf", width = 10, height = 10)
 
       nCount.vln <- VlnPlot(object=obj, features="nCount_RNA", pt.size=0.001) + ggtitle("nCount_RNA distribution per cluster")
-      ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, "_nCount.pdf"), plot = nCount.vln, device = "pdf", width = 10, height = 10)
+      ggsave(filename = paste0(plot_dir, sv.name, "_", res, "_nCount.pdf"), plot = nCount.vln, device = "pdf", width = 10, height = 10)
 
       nFeature.vln <- VlnPlot(object=obj, features="nFeature_RNA", pt.size=0.001) + ggtitle("nFeature_RNA distribution per cluster")
-      ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, "_nFeature.pdf"), plot = nFeature.vln, device = "pdf", width = 10, height = 10)
+      ggsave(filename = paste0(plot_dir, sv.name, "_", res, "_nFeature.pdf"), plot = nFeature.vln, device = "pdf", width = 10, height = 10)
 
       percent.mt.vln <- VlnPlot(object=obj, features="percent.mt", pt.size=0.001) + ggtitle("Percentage mitochondrial genes per cluster")
-      ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, "_PercentMT_VLN.pdf"), plot =   percent.mt.vln, device = "pdf", width = 10, height = 10)
+      ggsave(filename = paste0(plot_dir, sv.name, "_", res, "_PercentMT_VLN.pdf"), plot =   percent.mt.vln, device = "pdf", width = 10, height = 10)
     }
 
     clustre <- clustree(x=seurat, prefix="RNA_snn_res.")
-    ggsave(filename = paste0(plot_dir, "/", sv.name, "_Clustree.pdf"), plot = clustre, device = "pdf", width = 10, height = 10)
+    ggsave(filename = paste0(plot_dir, sv.name, "_Clustree.pdf"), plot = clustre, device = "pdf", width = 10, height = 10)
   }
 
 }

a5c685af5028911d785b9ab9b39337ef010bdf15
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-02-28 10:45:49 +1100

    reorder directories

--- getSeuratObject.R
+++ getSeuratObject.R
@@ -13,11 +13,17 @@ source_url("https://raw.githubusercontent.com/Jeron1996/SingleCellFunctions/mast
 source_url("https://raw.githubusercontent.com/Jeron1996/SingleCellFunctions/master/191211-hashing_workflow.R")
 source_url("https://raw.githubusercontent.com/Jeron1996/SingleCellFunctions/master/191209-QC_plots.R")
 set.seed(160396)
-###Start workflow by filtering out empty droplets using e.drop command
+
+##Directories and variables needed
 raw_data_dirs <- paste0("R://Zilog-Cancer-TumourDevelopment/190510-HANSBRO1-SingleCellData/collection", c(1:6), "/RNA/output/outs/raw_feature_bc_matrix/")
 pro.name <- paste0("Collection", c(1:6))
 save.Dir <- paste0("G://Hansbro_data/ReRun_normalized/Emptydrop_Out/")
 save.Dir <- paste0("/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/Emptydrop_Out/")
+output_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/HashingOut/"
+seurat_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/SeuratObjects/"
+plot_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/Plots/"
+
+###Start workflow by filtering out empty droplets using e.drop command
 #Performs emptydrop for every collection
 for(dir in raw_data_dirs){
   name <- strsplit(x = dir, split = "/")[[1]][5]
@@ -32,7 +38,6 @@ e.files <- list.files(path = "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_no
 hash <- c("A0303", "A0304", "A0305", "A0306", "A0307", "A0308")
 hash_trans <- as.character(c("Sal/SPG", "Ova 1", "Ova/DEX", 'Sal/Cmu', "Ova/Cmu", "Ova/Cmu/DEX"))
 hashing_translation <- data.frame(hash, hash_trans, stringsAsFactors = F)
-output_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/HashingOut/"
 
 #Loops over all files of the emptydrop output and performs the hashing workflow to translate hashtags into samples and filter out droplets with double hashtags.
 for(file in e.files){
@@ -60,9 +65,6 @@ for(hashing_file in hashing_files){
 #Merge all hashtag outputs
 seurat_merged <- merge(x = seurat_list[[1]], y = c(seurat_list[[2]], seurat_list[[3]], seurat_list[[4]], seurat_list[[5]], seurat_list[[6]]))
 seurat_merged <- seurat_merged[, seurat_merged$percent.mt < 10]
-seurat_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/SeuratObjects/"
-plot_dir <- "/share/ScratchGeneral/jerven/Hansbro_data/ReRun_normalized/Plots/"
-resolution <- c(0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 1.2, 1.4)
 
 #Perform simple Seurat workflow, make plots that can be used for reference.
 seurat_merged <- NormalizeData(seurat_merged)
@@ -71,9 +73,12 @@ seurat_merged <- ScaleData(object = seurat_merged, features = rownames(seurat_me
 seurat_merged <- RunPCA(seurat_merged, features = VariableFeatures(object = seurat_merged))
 #Use first 20 dimensions for UMAP
 seurat_merged <- FindNeighbors(seurat_merged, dims = 1:15)
+
+resolution <- c(0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 1.2, 1.4)
 for(res in resolution){
   seurat_merged <- FindClusters(seurat_merged, resolution = res)
 }
+
 seurat_merged <- RunUMAP(seurat_merged, dims = 1:15)
 #Add Cell cycle information to the Seurat Object
 load("/share/ScratchGeneral/jerven/Hansbro_data/CellCylceGenes/cell.cyclegenes.rdata")

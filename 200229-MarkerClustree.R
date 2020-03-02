30a0b715a8861aa7145801c4a98eae67d8dbb6a2
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-03-02 16:15:49 +1100

    Initial commit

---
+++ 200229-MarkerClustree.R
@@ -0,0 +1,33 @@
+##This function makes a clustree, where the nodes are colored according to the mean expression of a gene of interest.
+#@Param seurat: Give a seurat object to use for clustree
+#@Param gene: Give gene of interest. If multiple genes are given the function will output a list of all plots
+library(clustree)
+library(Seurat)
+MarkerClustree <- function(seurat, gene){
+  #if length of gene is 1 (i.e. only one gene is given), then only ouput one plot. Otherwise make a list of plots
+  if(length(gene)==1){
+    #Make a data.frame with meta information from seurat object and gene of interest expression, then format column names
+    df <- data.frame(seurat@meta.data, seurat@assays$RNA@scale.data[gene, ])
+    colna <- colnames(df)
+    colna[grepl(pattern = "seurat.assays.RNA.scale.data.gene...", x = colna)] <- gene
+    colnames(df) <- colna
+    
+    #Create clustree and return it
+    plot <- clustree(x = df, prefix = "RNA_snn_res.", , node_colour=gene, node_colour_aggr = "mean")
+    return(plot)
+    
+  } else{
+    plot_list <- list() #Output list for final plots
+    for(gen in gene){
+      df <- data.frame(seurat@meta.data, seurat@assays$RNA@scale.data[gen, ])
+      colna <- colnames(df)
+      colna[grepl(pattern = "seurat.assays.RNA.scale.data.gen...", x = colna)] <- gen
+      colnames(df) <- colna
+      
+      #Create clustree and save output in list
+      plot <- clustree(x = df, prefix = "RNA_snn_res.", , node_colour=gen, node_colour_aggr = "mean")
+      plot_list[[gen]] <- plot
+    }
+    return(plot_list)
+  }
+}

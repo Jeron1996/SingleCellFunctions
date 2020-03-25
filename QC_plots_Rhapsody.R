bf05db09ae89537bbb156c8c9f0b252869471936
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-03-25 15:41:46 +1100

    Remove unnecessary plots

--- QC_plots_Rhapsody.R
+++ QC_plots_Rhapsody.R
@@ -18,17 +18,8 @@ plots_cluster <- function(seurat.object, save.name, dir){
   Nelbow <- ElbowPlot(object = obj, ndims = 30)
   ggsave(filename = paste0(plot_dir, "/", sv.name, "_withSeed_ElbowPlot.pdf"), plot = Nelbow, device = "pdf", width = 10, height = 10)
 
-  NsampleID <- DimPlot(object = obj, group.by = "SampleID", label = T)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_SampleID.pdf"), plot = NsampleID, device = "pdf", width = 10, height = 10)
-
   Norig.ident <- DimPlot(object = obj, group.by = "orig.ident", label = T)
   ggsave(filename = paste0(plot_dir, "/", sv.name, "_OrigIdent.pdf"), plot = Norig.ident, device = "pdf", width = 10, height = 10)
-
-  NCellCycle <- DimPlot(object = obj, group.by = "Phase", label = T)
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_CellCyclePhase.pdf"), plot = NCellCycle, device = "pdf", width = 10, height = 10)
-
-  NpercentMT <- FeaturePlot(object = obj, features = "percent.mt")
-  ggsave(filename = paste0(plot_dir, "/", sv.name, "_PercentMT.pdf"), plot = NpercentMT, device = "pdf", width = 10, height = 10)
 
   ## Plot DimPlots for all resolutions
   resolution <- names(obj@meta.data)
@@ -46,9 +37,6 @@ plots_cluster <- function(seurat.object, save.name, dir){
 
       nFeature.vln <- VlnPlot(object=obj, features="nFeature_RNA", pt.size=0.001) + ggtitle("nFeature_RNA distribution per cluster")
       ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, "_nFeature.pdf"), plot = nFeature.vln, device = "pdf", width = 10, height = 10)
-
-      percent.mt.vln <- VlnPlot(object=obj, features="percent.mt", pt.size=0.001) + ggtitle("Percentage mitochondrial genes per cluster")
-      ggsave(filename = paste0(plot_dir, "/", sv.name, "_", res, "_PercentMT_VLN.pdf"), plot =   percent.mt.vln, device = "pdf", width = 10, height = 10)
     }
 
     clustre <- clustree(x=obj, prefix="SCT_snn_res.")

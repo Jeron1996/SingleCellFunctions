6b49c078eb095d3b39e6dccb9c37ef681165557f
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-05-15 16:50:03 +1000

    Update 200515_Run_split_bam_parallel.sh

--- sh/200515_Run_split_bam_parallel.sh
+++ sh/200515_Run_split_bam_parallel.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
+#This submits the split bam script to a SGE scheduler
 qsub -cwd -V -N split_parallel -pe smp 10 -l mem_requested=10G,tmp_requested=50G -b y -j y './200515_split_bam_parallel.sh'

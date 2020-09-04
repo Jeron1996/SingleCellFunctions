2df1728f48a0c2dd4e3e63cd8679c6832cbfbaa4
Jeron1996 <46191575+Jeron1996@users.noreply.github.com>
2020-09-04 16:13:25 +1000

    Update 191202-emptydrop.R

--- R/191202-emptydrop.R
+++ R/191202-emptydrop.R
@@ -63,7 +63,7 @@ e.drop <- function(raw_matrix_counts, project.name, saveDir){
 
   ##Create Seurat objects from new data matrix files
   seurat.ok <- CreateSeuratObject(counts = counts.ok, project = as.character(project.name), min.cells = 3, min.features = 100)
-  seurat.ok <- subset(seurat.ok, subset = nFeature_RNA <= 4000)   #Cut off all cells with more than 4000 features
+  #seurat.ok <- subset(seurat.ok, subset = nFeature_RNA <= 4000)   #Cut off all cells with more than 4000 features
   seurat.empty <- CreateSeuratObject(counts = counts.empty, project = as.character(project.name), min.cells = 3)
 
   #Calculate percentage mitochondrial genes
